3914f5a3d72e46028cb098af98288129eb8b1883
nelsong44 <nelsong44@gmail.com>
2017-07-14 16:06:00 -0500

    created empty array and storing user input inside, repeating through array and displaying to DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,12 +2,18 @@ console.log('JS Sourced');
 
 //create app and link angular to it
 var myApp = angular.module('myApp', []);
-myApp.controller('OnloadController', function() {
+myApp.controller('LoadController', function() {
   //declare a var equal to our controller to access it and use it
   var control = this;
-  
+  control.userArray = [];
   control.displayInfo = function(userName, userMessage) {
+    // first approach...
+    // control.info = {userName: userName, userMessage: userMessage};
+
+    //second approach
     var info = {userName: userName, userMessage: userMessage};
     console.log(info);
+    control.userArray.push(info);
+    console.log(control.userArray);
   };
 });
