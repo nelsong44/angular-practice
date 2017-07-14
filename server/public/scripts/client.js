55700f2893760f46e79f2f2c8f9b473a82cf067b
nelsong44 <nelsong44@gmail.com>
2017-07-14 15:21:07 -0500

    created basic angular setup, user input fields, logging user input in console

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,13 @@
 console.log('JS Sourced');
+
+//create app and link angular to it
+var myApp = angular.module('myApp', []);
+myApp.controller('OnloadController', function() {
+  //declare a var equal to our controller to access it and use it
+  var control = this;
+  
+  control.displayInfo = function(userName, userMessage) {
+    var info = {userName: userName, userMessage: userMessage};
+    console.log(info);
+  };
+});
